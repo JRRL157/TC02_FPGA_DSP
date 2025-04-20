35dea4ddd0eee5ca2b2710f2a506ab4439bb9fa0
JRRL157 <jrrlino157@gmail.com>
2025-04-20 12:54:47 -0300

    Project 03
    Pushing Project 03, 4 bit Multiplier in Python and C, testing communication between PC and HPS over UDP protocol

---
+++ TC02_Proj_03/HPS/transferir_arquivos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PASTA_LOCAL="./multiplicadorUDP"
+IP_DESTINO="root@10.42.0.23"
+PASTA_RAIZ_PROJETO="TC_02_JRRL"
+PASTA_DESTINO=$PASTA_RAIZ_PROJETO/"multiplicadorUDP"
+echo "Subindo Arquivos do Projeto - $PASTA_LOCAL:"
+scp $PASTA_LOCAL/* $IP_DESTINO:/home/root/$PASTA_DESTINO
+echo "Subindo Bibliotecas comuns a todos os projetos:"
+scp ./INC/* $IP_DESTINO:/home/root/$PASTA_RAIZ_PROJETO/INC
+
+
---
+++ TC02_Proj_03/multiplicadorUDP.py
@@ -0,0 +1,53 @@
+import socket
+import numpy
+import struct
+
+def communication(msgFromClient: str, ip_address: str, port: int) -> int:
+    bytesToSend         = str.encode(msgFromClient)
+    serverAddressPort   = (ip_address, port)
+    bufferSize          = 20
+    #bufferSize          = len(bytesToSend)
+
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    print("ATENÇÃO! ENVIANDO PACOTE PARA A PORTA -", serverAddressPort)
+
+    print("Mensagem  a ser enviada:", msgFromClient)
+
+    print("Enviando a mensagem pelo socket criado")
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+    print("Esperando receber algo:")
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+    #num = struct.unpack('<I', msgFromServer[0][:4])[0]
+    msg = msgFromServer[0].decode('utf-8').rstrip('\x00')
+    print(f"Mensagem recebida: {msg}")
+
+    return int(msg)
+
+def multiplicar(num1: int, num2: int) -> int:
+    return num1 * num2;
+
+ip_address = "10.42.0.23"
+port = 9090
+
+quadro_final = {
+    'error': 0,
+    'total': 0,
+    'occurrence': []
+}
+for a in range(2,16):
+    for b in range(a,16):
+        res = multiplicar(a,b)
+        
+        msg = f"{a},{b}"
+        print(msg)
+        int_rec = communication(msg, ip_address, port)
+        
+        quadro_final['total'] += 1;
+        if int_rec != res:
+            quadro_final['error'] += 1
+            quadro_final['occurrence'].append((a, b, res, int_rec))
+
+print(quadro_final)
