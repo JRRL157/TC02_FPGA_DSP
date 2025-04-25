f97b38e43c812c641092790744b2b3740f84a50e
JRRL157 <jrrlino157@gmail.com>
2025-04-24 21:05:39 -0300

    Proj 2
    Removed unwanted LATCHES from the four bit multiplier

--- TC02_Proj_02/hdl_files/four_bit_multiplier.sv
+++ TC02_Proj_02/hdl_files/four_bit_multiplier.sv
@@ -20,35 +20,40 @@ module four_bit_multiplier(
         .state_o(curr_state)
     );
     
-    always @(*) begin
-        case(curr_state)
+    always @(curr_state) begin
+        A_reg = 8'b0;
+		  B_reg = 8'b0;
+		  Y = 8'b0;
+		  prod = '{default: 8'd0};
+		  done = 1'b0;
+		  case(curr_state)
             ST_IDLE: begin
-                A_reg <= {4'b0, A};
-                B_reg <= {4'b0, B};
-                done <= 1'b0;
+                A_reg = {4'b0, A};
+                B_reg = {4'b0, B};
+                done = 1'b0;
             end
             ST_COMPUTE_PROD0: begin
-                prod[0] <= B_reg[0] ? A_reg : 8'b0;
-                done <= 1'b0;
+                prod[0] = B_reg[0] ? A_reg : 8'b0;
+                done = 1'b0;
             end
             ST_COMPUTE_PROD1:
             begin
-                prod[1] <= B_reg[1] ? A_reg : 8'b0;
-                done <= 1'b0;
+                prod[1] = B_reg[1] ? A_reg : 8'b0;
+                done = 1'b0;
             end
             ST_COMPUTE_PROD2:
             begin
-                prod[2] <= B_reg[2] ? A_reg : 8'b0;
-                done <= 1'b0;
+                prod[2] = B_reg[2] ? A_reg : 8'b0;
+                done = 1'b0;
             end
             ST_COMPUTE_PROD3:
             begin
-                prod[3] <= B_reg[3] ? A_reg : 8'b0;
-                done <= 1'b0;
+                prod[3] = B_reg[3] ? A_reg : 8'b0;
+                done = 1'b0;
             end
             ST_END:
             begin
-                Y <= prod[0] + (prod[1] << 1) + (prod[2] << 2) + (prod[3] << 3);
+                Y = prod[0] + (prod[1] << 1) + (prod[2] << 2) + (prod[3] << 3);
 					 done <= 1'b1;
             end
         endcase
