bfb18d20e0501babd1159535ba39cb366d06b0c8
JRRL157 <jrrlino157@gmail.com>
2025-06-18 06:29:50 -0300

    [TC02 Final Project] Octave - One more worker

---
+++ TC02_Final_Project/Octave/main_hadamard_2nd.m
@@ -0,0 +1,169 @@
+% First of all, install the 'octave-communications' package from AUR
+
+% Main procedure
+clc;
+clear;
+pkg load communications;
+pkg load instrument-control
+
+% Prefixo
+directory = "images/";
+
+% Simulation type (0 for OFDM, 1 for OTFS)
+optimized = 0;
+
+file_suffix = 'simulação.png';
+
+% 3GPP Standard channel
+delays_EVA = [0, 30, 150, 310, 370, 710, 1090, 1730, 2510] * 1e-9;
+pdp_EVA = [0.0, -1.5, -1.4, -3.6, -0.6, -9.1, -7.0, -12.0, -16.9];
+
+delays_EPA = [0, 30, 70, 90, 110, 190, 410]*1e-9;
+pdp_EPA = [0.0, -1.0, -2.0, -3.0, -8.0, -17.2, -20.8];
+
+delays_ETU = [0, 50, 120, 200, 230, 500, 1600, 2300, 5000]*1e-9;
+pdp_ETU = [-1.0, -1.0, -1.0, 0.0, 0.0, 0.0, -3.0, -5.0, -7.0];
+
+channel_model_name = ["EVA", "EPA", "ETU"];
+
+%Multicarrier modulation schemes
+otfs_hadamard_str = "otfs_hadamard";
+otfs_walsh_str = "otfs_walsh";
+
+mod_schemes = {'OTFS-Hadamard', 'OTFS-Walsh'};
+
+MOD_SIZE = 4;
+
+% SNR
+SNR_step = 10; % Incremento de SNR em dB
+SNR_values = 10:SNR_step:20; % Vetor de valores de SNR
+
+% Number of Iterations
+num = 1000;
+
+% Initialize BER_values object for all modulation types
+BER_values = zeros(length(mod_schemes), length(SNR_values));
+
+
+%[1]Low-Complexity VLSI Architecture for OTFS Transceiver Under Multipath Fading Channel
+%[2]Performance Evaluation of OTFS Over Measured V2V Channels at 60 GHz
+%[3]A Robust and Low-Complexity Walsh-Hadamard Modulation for Doubly-Dispersive Channels
+
+% Define selected (idx, N, M, spd, fc, delta_f) tuples
+simulation_params = [
+    1, 16, 16, 500, 6e9, 30e3; %[1]
+
+    %2, 8, 8, 50, 60e9, 625000;%BW=5MHz, [2]
+    %3, 8, 64, 50, 60e9, 78125;%BW=5MHz, [2]
+    %4, 8, 8, 50, 60e9, 5e6;%BW=40MHz, [2]
+    %5, 8, 64, 50, 60e9, 625e3;%BW=40MHz, [2]
+    %6, 8, 8, 50, 60e9, 15e6; %BW=120MHz, [2]
+    7, 8, 64, 50, 60e9, 1.875e6; %BW=120MHz, [2]
+
+    %8, 16, 64, 300, 5.9e9, 78.125e3;%BW=5MHz [3]
+    %9, 4, 128, 300, 5.9e9, 78.125e3;%BW=10MHz [3]
+];
+
+for idx = 1:size(simulation_params)
+
+    C = simulation_params(idx, 1);
+    N = simulation_params(idx, 2);
+    M = simulation_params(idx, 3);
+    spd = simulation_params(idx, 4);
+    fc = simulation_params(idx, 5);
+    delta_f = simulation_params(idx, 6);
+
+    %Título do plot
+    plot_title = sprintf('Iterações=%d,N=%d,M=%d,spd(km/h)=%d,delta_f(KHz)=%.2f, f_c(GHz)=%.2f', num, N, M, spd, (delta_f/1e3), (fc/1e9));
+
+    for channel_model_selector = 1:1
+        delays_arr = [];
+        pdp_arr = [];
+        channel_model_name = "";
+        switch(channel_model_selector)
+            case 1
+                delays_arr = delays_EVA;
+                pdp_arr = pdp_EVA;
+                channel_model_name = "EVA";
+            case 2
+                delays_arr = delays_EPA;
+                pdp_arr = pdp_EPA;
+                channel_model_name = "EPA";
+            case 3
+                delays_arr = delays_ETU;
+                pdp_arr = pdp_ETU;
+                channel_model_name = "ETU";
+        end
+
+        mod_size = MOD_SIZE;
+        for type = 1:length(mod_schemes) % Loop over modulation schemes
+          % Loop sobre os valores de SNR
+          for snr_idx = 1:length(SNR_values)
+              SNR_db = SNR_values(snr_idx);
+              total_errors = 0;
+              total_bits = 0;
+
+              % Simule para o valor atual de SNR
+              for i = 1:num
+                  disp(['Type = ', num2str(type), ', SNR #',num2str(SNR_db), ', Iteration #', num2str(i)]);
+                  if (type == 1)
+                    [x, x_hat] = otfs_hadamard_2nd(N, M, spd, fc, delta_f, SNR_db, mod_size, delays_arr, pdp_arr, "127.0.0.1", 5016);
+                  elseif (type == 2)
+                    [x, x_hat] = otfs_walsh(N, M, spd, fc, delta_f, SNR_db, mod_size, delays_arr, pdp_arr);
+                  end
+
+                  % Verifique se x e x_hat têm o mesmo comprimento
+                  if length(x) ~= length(x_hat)
+                      %disp(['Temperature is:' num2str(UU(90)) 'After: ' num2str(timeInMinutes) ' minutes']);
+                      disp('x e x_hat devem ter o mesmo comprimento.');
+                      disp(['size(x):' num2str(length(x))]);
+                      disp(['size(x_hat)' num2str(length(x_hat))]);
+                      continue;
+                  end
+
+                  % Calcule o número de erros para esta execução
+                  errors = sum(x ~= x_hat);
+
+                  % Atualize os contadores totais
+                  total_errors = total_errors + errors;
+                  total_bits = total_bits + length(x);
+              end
+
+              % Calcule o BER para o valor atual de SNR
+              BER_values(type, snr_idx) = total_errors / total_bits;
+
+              % Write BER values to a file
+              filename = 'ber_values_2nd.txt';
+              file = fopen(filename, 'a'); % Open file in append mode
+              if file == -1
+                  error('Could not open file for writing.');
+              end
+              dateAndTime = datestr(now(), 'yyyy_mmmm_dd_HH-MM-SS');
+              fprintf(file, 'Date: %s: ,Scenario Index: %d, Type: %d, Modulation: %d-QAM, SNR: %f, BER: %e\n', dateAndTime, C, type, mod_size, SNR_db, BER_values(type, snr_idx));
+              fclose(file);
+          end
+        end
+
+        % Replace zeros in BER_values with a small positive value
+        BER_values(BER_values == 0) = 1e-10;
+
+        % Use a stable renderer
+        %set(gcf, 'renderer', 'painters'); % Or use graphics_toolkit('gnuplot');
+
+        % Plot BER vs. SNR
+        %fig = figure('visible', 'off');
+        %hold on;
+        %for type = 1:length(mod_schemes) % Adjust loop range to valid indices
+        %    semilogy(SNR_values, BER_values(type, :), '-o', 'LineWidth', 1.5, 'DisplayName', mod_schemes{type});
+        %end
+        %grid on;
+        %set(gca, 'YScale', 'log');
+        %xlabel('SNR (dB)');
+        %ylabel(sprintf('BER - [%d QAM, %s Channel Model]', mod_size, channel_model_name));
+        %legend('show');
+        %title(plot_title);
+        %dateAndTime = datestr(now(), 'yyyy_mmmm_dd_HH-MM-SS');
+        %fname = sprintf('%s%s_%s_%d-QAM.png', directory, dateAndTime, channel_model_name, mod_size);
+        %print(fname, '-dpng', '-r300'); % Save as PNG with 300 DPI resolution
+    end
+end
---
+++ TC02_Final_Project/Octave/otfs_hadamard_2nd.m
@@ -0,0 +1,128 @@
+function [x, x_hat2] = otfs_hadamard(N, M, spd, fc, delta_f, SNR_db, mod_size, delays_arr, pdp_arr, ip_addr, port)
+  Hn = hadamard(N);
+  Hm = hadamard(M);
+  Hn=Hn/norm(Hn);
+  Hm=Hm/norm(Hm);
+
+  %TODO: Please delete these later
+  T=1/delta_f;
+  c = 299792458;
+
+  delay_resolution = 1/(M*delta_f);
+  doppler_resolution = 1/(N*T);
+
+  % Generating OTFS frame
+  N_syms_per_frame = N*M;
+
+  random_syms = randi([0 mod_size-1], [N_syms_per_frame 1]);
+  tx_info_symbols = qammod(random_syms, mod_size);
+
+  X = reshape(tx_info_symbols, M, N);
+  x = reshape(X.', N*M, 1);
+
+  % OTFS Modulation
+  Im = eye(M);
+
+  P = zeros(N*M, N*M);
+  for j=1:N
+    for i=1:M
+      E=zeros(M,N);
+      E(i,j)=1;
+      P((j-1)*M+1:j*M,(i-1)*N+1:i*N)=E;
+    end
+  end
+
+  
+  %disp(X);
+  X_tf_r = udp_dwht(3, ip_addr, port, real(X));
+  X_tf_i = udp_dwht(3, ip_addr, port, imag(X));
+  X_tf = X_tf_r + (X_tf_i * 1i);
+  X_tf_ideal = Hm*X*Hn';
+  disp(X_tf_ideal - X_tf);
+  
+  %X_til_ideal = Hm' * X_tf;
+  %disp(X_tf);
+  X_til_r = udp_dwht(2, ip_addr, port, real(X_tf));
+  X_til_i = udp_dwht(2, ip_addr, port, imag(X_tf));
+  X_til = X_til_r + (X_til_i * 1i);
+  %disp(X_til);
+
+  s = reshape(X_til, 1, N*M);
+
+  % Channel
+  max_ue_spd_mps = spd / 3.6;
+  nu_max = (max_ue_spd_mps*fc)/c;
+  k_max = nu_max/doppler_resolution;
+
+  % Generate standard channel parameters
+  delays = delays_arr;
+  pdp=pdp_arr;
+  pdp_linear = 10.^(pdp/10);
+  pdp_linear = pdp_linear/sum(pdp_linear);
+  taps=length(pdp_linear);
+
+  g_i = sqrt(pdp_linear).*(sqrt(1/2)*(randn(1,taps)+1i*randn(1,taps)));
+  l_i = round(delays./delay_resolution);
+  k_i = (k_max*cos(2*pi*rand(1,taps))); %%%%% --------- %%%%%%
+
+  % Generate discrete delay-time channel coefficients and matrix_type
+  z = exp(1i*2*pi/N/M);
+  delay_spread = max(l_i);
+
+  gs = zeros(delay_spread+1, N*M);
+  for q=0:N*M-1
+    for i=1:taps
+      gs(l_i(i)+1,q+1)=gs(l_i(i)+1,q+1) + g_i(i)*z^(k_i(i)*(q-l_i(i)));
+    end
+  end
+
+  G = zeros(N*M, N*M);
+  for q=0:N*M-1
+    for ell=0:delay_spread
+      if(q >= ell)
+        G(q+1,q-ell+1)=gs(ell+1,q+1);
+      end
+    end
+  end
+
+  H_til = P*G*P.';
+  H=kron(Im,Hn)*(P' * G * P)*kron(Im,Hn');
+
+  % Generate r by passing the Tx signal through the channel
+  r=G*s.';
+
+  % Add AWGN
+  Es = mean(abs(x).^2);
+  SNR=10.^(SNR_db/10);
+
+  sigma_w_2 = (Es / SNR); % Normalize by number of symbols
+  noise = sqrt(sigma_w_2 / 2)*(randn(N*M, 1) + 1i*randn(N*M, 1));
+
+  r = r + noise;
+
+  % OTFS demodulation
+  Y_til = reshape(r, M, N);
+  %Y_tf = Hm * Y_til;
+
+  Y_tf_r = udp_dwht(2, ip_addr, port, real(Y_til));
+  Y_tf_i = udp_dwht(2, ip_addr, port, imag(Y_til));
+  Y_tf = Y_tf_r + (Y_tf_i * 1i);
+
+  %Y = Hm' * Y_tf * Hn;
+  Y_r = udp_dwht(3, ip_addr, port, real(Y_tf));
+  Y_i = udp_dwht(3, ip_addr, port, imag(Y_tf));
+  Y = Y_r + (Y_i * 1i);
+
+  % OTFS delay-doppler LMMSE detection
+  y = reshape(Y.', N*M, 1);
+  x_hat = (((H' * H + sigma_w_2*eye(size(H)))^(-1)) * H') * y;
+
+  if isnan(x_hat)
+    x_hat = 0;
+    x_hat2 = 0;
+  else
+    x_hat = qamdemod(x_hat, mod_size);
+    x_hat2 = qammod(x_hat, mod_size);
+  end
+end
+
