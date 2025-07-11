ecb299b877fdc5902b4ae74ca4f6c265392331dd
JRRL157 <jrrlino157@gmail.com>
2025-07-11 05:54:22 -0300

    [TC02 Final Project] HDL, Testbench and IVerilog script for length 128

--- TC02_Final_Project/FPGA/DE1/hdl_files/fwht/L128.sv
+++ TC02_Final_Project/FPGA/DE1/hdl_files/fwht/L128.sv
@@ -1,11 +1,14 @@
 module L128(
-    input  logic        clk,
-    input  logic        rst,
-    input  logic [31:0] x [127:0],
-    output logic [31:0] y [127:0]
+    input       clk,
+    input       rst,
+    input  wire [31:0] x [127:0],
+    output wire [31:0] y [127:0]
 );
-    reg [31:0] a_upper[64];
-    reg [31:0] a_lower[64];
+    reg [31:0] a_upper [63:0];
+    reg [31:0] a_lower [63:0];
+    wire [31:0] y_upper [63:0];
+    wire [31:0] y_lower [63:0];
+
     integer i;
 
     always @(posedge clk or posedge rst) begin
@@ -23,16 +26,26 @@ module L128(
         end
     end
 
+    genvar gi;
+    generate
+        for (gi = 0; gi < 64; gi++) begin : y_assign
+            assign y[gi] = y_upper[gi];
+            assign y[gi + 64] = y_lower[gi];
+        end
+    endgenerate
+
     L64 l64_upper(
-        .clk(clk), .rst(rst),
+        .clk(clk),
+        .rst(rst),
         .x(a_upper),
-        .y(y[63:0])
+        .y(y_upper)
     );
 
     L64 l64_lower(
-        .clk(clk), .rst(rst),
+        .clk(clk),
+        .rst(rst),
         .x(a_lower),
-        .y(y[127:64])
+        .y(y_lower)
     );
 
 endmodule
