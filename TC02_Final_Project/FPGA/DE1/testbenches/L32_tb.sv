2ffda8445a3df563f21fb3a2d021085d98921fc4
JRRL157 <jrrlino157@gmail.com>
2025-07-14 16:41:52 -0300

    [TC02 Final Project] Updated testbench for Length 32

--- TC02_Final_Project/FPGA/DE1/testbenches/L32_tb.sv
+++ TC02_Final_Project/FPGA/DE1/testbenches/L32_tb.sv
@@ -2,44 +2,38 @@
 
 module L32_tb();
 
-    // Testbench signals for N=32
     logic [31:0] x [31:0];
     wire [31:0] y [31:0];
     logic [31:0] expected_y [31:0];
 
     logic clk;
-    logic rst; // Reset signal
+    logic rst;
     integer i;
     reg has_error;
 
     parameter CLK_PERIOD = 10ns; // 10ns period -> 100 MHz clock
     parameter CLK_HALF_PERIOD = CLK_PERIOD / 2;
 
-    // Clock generator
     always #CLK_HALF_PERIOD clk = ~clk;
 
-    // File descriptors
     integer input_file_fd;
     integer output_file_fd;
 
-    // Test counters
     int test_count = 0;
     int errors = 0;
 
     // Instantiate the L32 module
-    L32 fwht_L32 (
+    LN #(.N(32)) fwht_L32 (
         .clk(clk),
         .rst(rst),
         .x(x),
         .y(y)
     );
-    
+
     initial begin
-        // --- Initialization and Reset ---
         clk = 0;
         rst = 1; // Assert reset
 
-        // Open sample files
         input_file_fd = $fopen("../../../HPS/samples/input_samples_32.txt","r");
         if (input_file_fd == 0) begin
             $error("Error: could not open input_samples_32.txt!");
@@ -60,11 +54,11 @@ module L32_tb();
 
         // --- Test Loop ---
         while (!$feof(input_file_fd) && !$feof(output_file_fd)) begin
-            // Read 32 inputs
+
             if ($fscanf(input_file_fd, "%h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h\n", 
                          x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14], x[15],
                          x[16], x[17], x[18], x[19], x[20], x[21], x[22], x[23], x[24], x[25], x[26], x[27], x[28], x[29], x[30], x[31]) == 32) begin
-                // Read 32 expected outputs
+
                 if ($fscanf(output_file_fd, "%h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h\n",
                              expected_y[0], expected_y[1], expected_y[2], expected_y[3], expected_y[4], expected_y[5], expected_y[6], expected_y[7],
                              expected_y[8], expected_y[9], expected_y[10], expected_y[11], expected_y[12], expected_y[13], expected_y[14], expected_y[15],
@@ -72,7 +66,7 @@ module L32_tb();
                              expected_y[24], expected_y[25], expected_y[26], expected_y[27], expected_y[28], expected_y[29], expected_y[30], expected_y[31]) == 32) begin
                     
                     test_count++;
-                    #100; // Wait for the DUT to compute the result (adjust if latency is higher for L32)
+                    #100;
 
                     $display("\n--- Test Case %0d ---", test_count);
                     has_error = 0;
